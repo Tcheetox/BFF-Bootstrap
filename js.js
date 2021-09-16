ca215d564077382161e97a3b66e5c4ecb6a03bbd
KRE <tcheetox@gmail.com>
2021-09-16 13:14:48 +0200

    Cleanup

--- js.js
+++ js.js
@@ -1,127 +1,97 @@
 // Dropdown with checkboxes
-var dropdownMenuMovie = document.getElementById('dropdown-menu-movie')
-var dropdownMenuTitle = document.getElementsByClassName('dropdown-menu-title')[0]
-var dropdownMenuMovieItems = document.getElementById('dropdown-menu-movie-items')
+const dropdownMenuMovie = document.getElementById('dropdown-menu-movie')
+const dropdownMenuTitle = document.getElementsByClassName('dropdown-menu-title')[0]
+const dropdownMenuMovieItems = document.getElementById('dropdown-menu-movie-items')
+
 // Ensure proper width of dropdown movie menu (important to manage overflow with CSS)
+const onWindowManipulation = () => {
+	if (dropdownMenuTitle && dropdownMenuMovieItems && dropdownMenuMovie) {
+		dropdownMenuTitle.style.width = dropdownMenuMovie.offsetWidth - 35 + 'px'
+		dropdownMenuMovieItems.style.width = dropdownMenuMovie.offsetWidth + 'px'
+	}
+}
 onWindowManipulation()
 window.addEventListener('resize', onWindowManipulation)
-function onWindowManipulation() {
-	dropdownMenuTitle.style.width = dropdownMenuMovie.offsetWidth - 35 + 'px'
-	dropdownMenuMovieItems.style.width = dropdownMenuMovie.offsetWidth + 'px'
-}
-
-// Bind movie checkboxes to selection event
-var moviesCheckboxes = document.getElementsByClassName('movie-checkbox')
-Array.from(moviesCheckboxes).forEach(function (element, index, array) {
-	element.addEventListener('change', onMovieSelectionChange)
-})
 
 // Movie selection event
-onMovieSelectionChange()
-function onMovieSelectionChange() {
+const moviesCheckboxes = document.getElementsByClassName('movie-checkbox')
+const onMovieSelectionChange = () => {
 	dropdownMenuTitle.style.width = dropdownMenuMovie.offsetWidth - 35 + 'px'
 	var title = 'Choose movie(s)...'
 	var newTitle = ''
-	Array.from(moviesCheckboxes).forEach(function (element, index, array) {
-		if (element.checked == true) {
-			if (newTitle == '') {
-				newTitle = element.parentElement.textContent
-			} else {
-				newTitle = newTitle.concat('-', element.parentElement.textContent)
-			}
-		}
+	Array.from(moviesCheckboxes).forEach(el => {
+		if (el.checked) newTitle = newTitle === '' ? el.parentElement.textContent : newTitle.concat('-', el.parentElement.textContent)
 	})
 	if (newTitle == '') {
 		newTitle = title
 		dropdownMenuMovie.setAttribute('required', '')
-	} else {
-		dropdownMenuMovie.removeAttribute('required', '')
-	}
+	} else dropdownMenuMovie.removeAttribute('required', '')
 	dropdownMenuTitle.textContent = newTitle
+}
+onMovieSelectionChange()
+
+// Bind movie checkboxes to selection event
+if (moviesCheckboxes) {
+	Array.from(moviesCheckboxes).forEach(el => el.addEventListener('change', onMovieSelectionChange))
 }
 
 // Video preview
 // Change button size based on resolution
 if (screen.width < 768) {
-	var videoControlButtonIcon = document.querySelector('i.video-control-button')
-	videoControlButtonIcon.classList.remove('fa-4x')
-	videoControlButtonIcon.classList.add('fa-2x')
+	const videoControlButtonIcon = document.querySelector('i.video-control-button')
+	if (videoControlButtonIcon) {
+		videoControlButtonIcon.classList.remove('fa-4x')
+		videoControlButtonIcon.classList.add('fa-2x')
+	}
 	document.documentElement.style.setProperty('--video-control-icon-half-size', '16px')
 }
 
-// Bind elements with their event
-var carousel = document.getElementById('carouselMoviePreview')
-carousel.addEventListener('mouseenter', onVideoMouseEnter)
-carousel.addEventListener('mouseleave', onVideoMouseLeave)
-var videoControlButton = document.getElementsByClassName('video-control-button')[0]
-videoControlButton.addEventListener('click', function () {
-	onVideoClick()
-})
-
-bindVideosEnd()
-function bindVideosEnd() {
-	var videos = document.getElementsByTagName('video')
-	Array.from(videos).forEach(function (element, index, array) {
-		element.addEventListener('ended', function () {
-			stopVideo(element)
-		})
-	})
-}
-
-bindCarouselLinks()
-function bindCarouselLinks() {
-	Array.from(document.getElementsByClassName('carousel-link')).forEach(function (element, index, array) {
-		element.addEventListener('click', function () {
-			stopAllRunningVideos()
-		})
-	})
-}
-
-// Event functions
-function onVideoMouseEnter() {
-	videoControlButton.setAttribute('style', 'opacity:0.8; -moz-opacity:0.8; filter:alpha(opacity=80)')
-}
-function onVideoMouseLeave() {
-	videoControlButton.setAttribute('style', 'opacity:0; -moz-opacity:0; filter:alpha(opacity=0)')
-}
-
-var runningVideos = []
-function stopAllRunningVideos() {
-	Array.from(runningVideos).forEach(function (video, index, array) {
-		if (video.paused == false) {
-			stopVideo(video)
-		}
-	})
-	runningVideos = []
-}
-
-function onVideoClick(video = -1) {
+const onVideoClick = (video = -1) => {
 	// Determine proper video element
 	let videoElement
-	if (video == -1) {
-		if (document.getElementsByClassName('carousel-item-next').length == 0) {
-			videoElement = document
-				.getElementsByClassName('carousel-item active')[0]
-				.getElementsByTagName('video')[0]
-		} else {
-			videoElement = document.getElementsByClassName('carousel-item-next')[0].getElementsByTagName('video')[0]
-		}
-	} else {
-		videoElement = document.getElementsByTagName('video')[video]
-	}
+	if (video === -1) {
+		if (document.getElementsByClassName('carousel-item-next').length === 0)
+			videoElement = document.getElementsByClassName('carousel-item active')[0].getElementsByTagName('video')[0]
+		else videoElement = document.getElementsByClassName('carousel-item-next')[0].getElementsByTagName('video')[0]
+	} else videoElement = document.getElementsByTagName('video')[video]
 
 	// Take actions
-	if (runningVideos.includes(videoElement)) {
-		stopAllRunningVideos()
-	} else {
+	if (runningVideos.includes(videoElement)) stopAllRunningVideos()
+	else {
 		stopAllRunningVideos()
 		runningVideos.push(videoElement)
 		playVideo(videoElement)
 	}
 }
 
+// Bind elements with their event
+const carousel = document.getElementById('carouselMoviePreview')
+const videoControlButton = document.getElementsByClassName('video-control-button')[0]
+if (videoControlButton) {
+	videoControlButton.addEventListener('click', () => onVideoClick())
+	if (carousel) {
+		carousel.addEventListener('mouseenter', () =>
+			videoControlButton.setAttribute('style', 'opacity:0.8; -moz-opacity:0.8; filter:alpha(opacity=80)')
+		)
+		carousel.addEventListener('mouseleave', () =>
+			videoControlButton.setAttribute('style', 'opacity:0; -moz-opacity:0; filter:alpha(opacity=0)')
+		)
+	}
+}
+
+let runningVideos = []
+const stopAllRunningVideos = () => {
+	Array.from(runningVideos).forEach(video => {
+		if (video && !video.paused) stopVideo(video)
+	})
+	runningVideos = []
+}
+
+Array.from(document.getElementsByTagName('video')).forEach(el => el.addEventListener('ended', () => stopVideo(el)))
+Array.from(document.getElementsByClassName('carousel-link')).forEach(el => el.addEventListener('click', stopAllRunningVideos))
+
 // Actions
-function playVideo(videoElement) {
+const playVideo = videoElement => {
 	// Stop bootstrap carousel
 	$('#carouselMoviePreview').carousel('pause')
 	// Change video icon
@@ -135,7 +105,7 @@ function playVideo(videoElement) {
 	toggleMoviesCaption()
 }
 
-function stopVideo(videoElement) {
+const stopVideo = videoElement => {
 	// Change video icon
 	videoControlButton.classList.remove('fa-pause-circle')
 	videoControlButton.classList.add('fa-play-circle')
@@ -151,85 +121,64 @@ function stopVideo(videoElement) {
 }
 
 // Hide video captions if video is started and mouse doesn't move
-var timeout
-var carouselIndicators = document.getElementsByClassName('carousel-indicators')[0]
-function toggleMoviesCaption() {
+let timeout
+const carouselIndicators = document.getElementsByClassName('carousel-indicators')[0]
+const toggleMoviesCaption = () => {
 	clearTimeout(timeout)
 	// Show captions
-	if (carouselIndicators.style.visibility != 'visible') {
+	if (carouselIndicators && carouselIndicators.style.visibility != 'visible') {
 		carouselIndicators.style.visibility = 'visible'
-		Array.from(document.getElementsByClassName('carousel-caption')).forEach(function (caption, index, array) {
-			caption.style.visibility = 'visible'
-		})
+		Array.from(document.getElementsByClassName('carousel-caption')).forEach(caption => (caption.style.visibility = 'visible'))
 	}
-	timeout = setTimeout(function () {
+	timeout = setTimeout(() => {
 		// Hide captions
-		if (carouselIndicators.style.visibility != 'hidden') {
+		if (carouselIndicators && carouselIndicators.style.visibility != 'hidden') {
 			carouselIndicators.style.visibility = 'hidden'
-			Array.from(document.getElementsByClassName('carousel-caption')).forEach(function (
-				caption,
-				index,
-				array
-			) {
-				caption.style.visibility = 'hidden'
-			})
+			Array.from(document.getElementsByClassName('carousel-caption')).forEach(caption => (caption.style.visibility = 'hidden'))
 		}
 	}, 3000)
 }
 
 // Bind play trailer buttons
-bindPlayTrailerLinks()
-function bindPlayTrailerLinks() {
-	Array.from(document.getElementsByClassName('trailer-indicator')).forEach(function (element, index, array) {
-		element.addEventListener('click', function () {
-			carousel.scrollIntoView({ behavior: 'smooth', block: 'end' })
-			var video = element.getAttribute('data-slide-to')
-			document.querySelectorAll('.carousel-indicators li')[video].click()
-			setTimeout(function () {
-				onVideoClick(video)
-			}, 1500)
-		})
+Array.from(document.getElementsByClassName('trailer-indicator')).forEach(element =>
+	element.addEventListener('click', () => {
+		carousel.scrollIntoView({ behavior: 'smooth', block: 'end' })
+		const video = element.getAttribute('data-slide-to')
+		document.querySelectorAll('.carousel-indicators li')[video].click()
+		setTimeout(() => onVideoClick(video), 1500)
 	})
-}
+)
 
 // MODAL: show only if form is submitted
-document.getElementById('registration-form').addEventListener('submit', function (event) {
+document.getElementById('registration-form').addEventListener('submit', event => {
 	$('#registration-modal').modal('show')
 	event.preventDefault()
 })
 
 // Navbar logo management
-var littleLogo = document.querySelector('.little-logo')
-var navBrand = document.querySelector('.navbar-brand')
-navBrand.addEventListener('mouseenter', function () {
-	littleLogo.src = 'pictures/logos/littleWhite.jpg'
-})
-navBrand.addEventListener('mouseleave', function () {
-	littleLogo.src = 'pictures/logos/littleBlack.jpg'
-})
+const littleLogo = document.querySelector('.little-logo')
+const navBrand = document.querySelector('.navbar-brand')
+if (navBrand && littleLogo) {
+	navBrand.addEventListener('mouseenter', () => (littleLogo.src = 'pictures/logos/littleWhite.jpg'))
+	navBrand.addEventListener('mouseleave', () => (littleLogo.src = 'pictures/logos/littleBlack.jpg'))
+}
 
 // Appear and disappear on scroll
-window.onscroll = function () {
-	scrollFunction()
-}
-var navLogo = document.querySelector('.navbar-brand')
-var jumbo = document.querySelector('.jumbotron')
-scrollFunction()
-function scrollFunction() {
+const navLogo = document.querySelector('.navbar-brand')
+const jumbo = document.querySelector('.jumbotron')
+const scrollFunction = () => {
 	if (document.body.scroll > jumbo.clientHeight || document.documentElement.scrollTop > jumbo.clientHeight) {
-		if (navLogo.style.visibility != 'visible') {
+		if (navLogo && navLogo.style.visibility != 'visible') {
 			navLogo.style.visibility = 'visible'
 			navLogo.style.opacity = 1
 		}
-	} else if (navLogo.style.visibility != 'hidden') {
+	} else if (navLogo && navLogo.style.visibility != 'hidden') {
 		navLogo.style.opacity = 0
-		setTimeout(function () {
-			navLogo.style.visibility = 'hidden'
-		}, 700)
+		setTimeout(() => (navLogo.style.visibility = 'hidden'), 700)
 	}
 }
+scrollFunction()
+window.onscroll = scrollFunction
 
 // Menu should collapse after click on mobile
-$('.navbar-nav li a').click(function (event) {
-	$('.navbar-collapse').collapse('hide')
-})
+$('.navbar-nav li a').click(() => $('.navbar-collapse').collapse('hide'))
